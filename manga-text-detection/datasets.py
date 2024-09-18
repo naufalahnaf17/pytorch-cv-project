603d4c7b142a356599852de9869b5fcdab9eae13
naufalahnaf17 <naufalahnaf37@gmail.com>
2024-09-18 10:59:31 +0700

    Update train and datasets

--- manga-text-detection/datasets.py
+++ manga-text-detection/datasets.py
@@ -1,1 +1,85 @@
-# Datasets Script (soon)
+import os
+import zipfile
+from torchvision.transforms import functional as F
+from torch.utils.data import DataLoader
+from torchvision.datasets import CocoDetection
+import torch
+
+def download_dataset():
+    if os.path.isdir("./data") == False:
+        command_kaggle = "kaggle datasets download -d naufalahnaf17/manga-text-detection -p data/"
+        os.system(command_kaggle)
+
+        zip_file_path = "./data/manga-text-detection.zip"
+        extract_folder = "./data"
+
+        with zipfile.ZipFile(zip_file_path,'r') as zip_ref:
+            zip_ref.extractall(extract_folder)
+
+def get_dataloader(batch_size=2):
+    dataset_path = "./data/"
+
+    train_path = os.path.join(dataset_path,'train')
+    valid_path = os.path.join(dataset_path,'valid')
+    test_path = os.path.join(dataset_path,'test')
+
+    train_anno = os.path.join(train_path,'_annotations.coco.json')
+    valid_anno = os.path.join(valid_path,'_annotations.coco.json')
+    test_anno = os.path.join(test_path,'_annotations.coco.json')
+
+    class MangaDataset(CocoDetection):
+        def __init__(self,root,anno,transform=None):
+            super(MangaDataset,self).__init__(root,anno)
+            self.transform = transform
+        
+        def __getitem__(self,index):
+            image,target = super(MangaDataset,self).__getitem__(index)
+            if self.transform is not None:
+                image = self.transform(image)
+            
+            boxes = []
+            labels = []
+            
+            for obj in target:
+                xmin,ymin,width,height = obj['bbox']
+                if width > 0 and height > 0:
+                    boxes.append([xmin,ymin,xmin+width,ymin+height])
+                    labels.append(obj['category_id'])
+            
+            target = {
+                "boxes" : torch.tensor(boxes,dtype=torch.float32),
+                "labels" : torch.tensor(labels,dtype=torch.int64),
+                "image_id" : torch.tensor(target[0]["image_id"],dtype=torch.int64)
+            }
+            
+            return image,target
+            
+    def transform(image):
+        return F.to_tensor(image)
+            
+    train_data = MangaDataset(train_path,train_anno,transform=transform)
+    valid_data = MangaDataset(valid_path,valid_anno,transform=transform)
+    test_data  = MangaDataset(test_path,test_anno,transform=transform)
+
+    train_loader = DataLoader(
+        train_data,
+        batch_size=batch_size,
+        shuffle=True,
+        collate_fn=lambda x: tuple(zip(*x))
+    )
+
+    valid_loader = DataLoader(
+        valid_data,
+        batch_size=batch_size,
+        shuffle=False,
+        collate_fn=lambda x: tuple(zip(*x))
+    )
+
+    test_loader = DataLoader(
+        test_data,
+        batch_size=batch_size,
+        shuffle=False,
+        collate_fn=lambda x: tuple(zip(*x))
+    )
+
+    return train_loader,valid_loader,test_loader,train_data.coco.cats
--- manga-text-detection/train.py
+++ manga-text-detection/train.py
@@ -1,1 +1,72 @@
-# Train Script (soon)
+import torch
+import torchvision
+from tqdm import tqdm
+from datasets import download_dataset,get_dataloader
+from torchvision.models.detection.faster_rcnn import fasterrcnn_resnet50_fpn,FasterRCNN_ResNet50_FPN_Weights
+
+def main():
+    # seed for reproducibility
+    torch.manual_seed(42)
+
+    # hyperparameters (tune this hyperparameter with your config)
+    batch_size = 2
+    learning_rate = 1e-3
+    epochs = 25
+
+    # setup dataloader
+    train_loader,valid_loader,test_loader,data_classes = get_dataloader(batch_size=batch_size)
+
+    # prepare model + fine tune
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = fasterrcnn_resnet50_fpn(weights=FasterRCNN_ResNet50_FPN_Weights.DEFAULT)
+    num_classes = len(data_classes) + 1
+    input_features = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = torchvision.models.detection.faster_rcnn.FastRCNNPredictor(input_features, num_classes)
+    model.to(device)
+
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params,lr=0.005,momentum=0.9,weight_decay=0.0005)
+
+    for epoch in range(epochs):
+        model.train()
+        running_loss = 0.0
+        for images,targets in tqdm(train_loader,desc=f"Training Epochs : {epoch + 1}"):
+            images = list(image.to(device) for image in images)
+            targets = [{ k : v.to(device) for k,v in t.items() } for t in targets]
+            
+            optimizer.zero_grad()
+            pred = model(images,targets)
+            losses = sum(loss for loss in pred.values())
+
+            running_loss += losses.item()
+            
+            losses.backward()
+            optimizer.step()
+
+        # skip validation 
+    
+    # save model
+    model.eval()
+    onnx_path = "manga-text-detection.onnx"
+    dummy_input = torch.randn(1, 3, 512, 512).to(device)
+
+    torch.onnx.export(
+        model,               
+        dummy_input,         
+        onnx_path,           
+        export_params=True,  
+        opset_version=20,    
+        do_constant_folding=True,  
+        input_names=['input'],   
+        output_names=['output'], 
+        dynamic_axes={'input' : {0 : 'batch_size'},'output' : {0 : 'batch_size'}})
+
+    print(f"Model Saved : {onnx_path}")
+
+
+if __name__ == "__main__":
+    # Download dataset if not exists
+    download_dataset()
+
+    # Start training model function
+    main()
